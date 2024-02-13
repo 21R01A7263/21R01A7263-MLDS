228973db4b1c0b0a78fd2f92eb96b80b995079b4
Gaurav Sai Teja <128503039+21R01A7263@users.noreply.github.com>
2024-02-13 11:25:25 +0530

    Week3

--- ID3/id3.py
+++ ID3/id3.py
@@ -1,81 +1,43 @@
+import matplotlib.pyplot as plt
 import pandas as pd
-df=pd.read_csv("lab2dataset.csv")
-print(df)
+import numpy as np
+
+def kernel(point, xmat, k):
+    m,n = np.shape(xmat)
+    weights = np.mat(np.eye((m)))
+    for j in range(m):
+        diff = point - X[j]
+        weights[j,j] = np.exp(diff*diff.T/(-2.0*k**2))
+    return weights
+
+def localWeight(point, xmat, ymat, k):
+    wei = kernel(point,xmat,k)
+    return (X.T*(wei*X)).I*(X.T*(wei*ymat.T))
+     
+def localWeightRegression(xmat, ymat, k):
+    m,n = np.shape(xmat)
+    ypred = np.zeros(m)
+    for i in range(m):
+        ypred[i] = xmat[i]*localWeight(xmat[i],xmat,ymat,k)
+    return ypred
+       
+data = pd.read_csv('10-dataset.csv')
+bill = np.array(data.total_bill)
+tip = np.array(data.tip)
  
+mbill = np.mat(bill)
+mtip = np.mat(tip)
 
-def entropy(probs):
- import math
- return sum(-prob*math.log(prob,2) for prob in probs)
+m= np.shape(mbill)[1]
+one = np.mat(np.ones(m))
+X = np.hstack((one.T,mbill.T))
 
-def entropy_of_list(a_list):
- from collections import Counter
- cnt = Counter (x for x in a_list)
- num_instances =len(a_list)
- probs=[x/num_instances for x in cnt.values()]
- return entropy(probs)
+ypred = localWeightRegression(X,mtip,0.5)
+SortIndex = X[:,1].argsort(0)
+xsort = X[SortIndex][:,0]
 
-total_entropy= entropy_of_list(df['PlayTime'])
-print(total_entropy)
-
-def information_gain(df,split_attribute_name, target_attribute_name, trace=0):
-
-  df_split =df.groupby(split_attribute_name)
-  for name,group in df_split: 
-    nobs=len(df.index)*1.0
-    df_agg_ent=df_split.agg({target_attribute_name: [entropy_of_list,lambda x: len(x)/nobs] })[target_attribute_name]
-
-    avg_info=sum(df_agg_ent['entropy_of_list'] * df_agg_ent['<lambda_0>'])
-    old_entropy=entropy_of_list(df[target_attribute_name])
-    return old_entropy-avg_info   
-
-def id3DT(df, target_attribute_name, attribute_names, default_class=None):
-  from collections import Counter
-  cnt = Counter(x for x in df[target_attribute_name])
-  if len(cnt)==1:
-     return next(iter(cnt))
-  elif df.empty or (not attribute_names):
-     return default_class
-  else:
-     default_class =max(cnt.keys())
-
-     gainz=[information_gain(df,attr, target_attribute_name) for attr in attribute_names]
-     index_of_max=gainz.index(max(gainz))
-     best_attr=attribute_names[index_of_max]
-     tree={best_attr:{}}
-     remaining_attributes_names=[i for i in attribute_names if i != best_attr]
-     for attr_val, data_subset in df.groupby(best_attr):
-      subtree=id3DT(data_subset,target_attribute_name,remaining_attributes_names,default_class)
-      tree[best_attr][attr_val]=subtree
-  return tree
-
-
-attribute_names=list(df.columns)
-attribute_names.remove('PlayTime')
-from pprint import pprint
-tree= id3DT(df,'PlayTime',attribute_names)
-print("The Resultant Decision Tree is ")
-pprint(tree)
-attribute=next(iter(tree))
-print("Best Attribute: \n", attribute)
-print("Tree Keys\n ", tree[attribute].keys())
- 
-
-def classify(instance, tree, default=None):
-  attribute=next(iter(tree))
-  print("Key:",tree.keys())
-  print("Attribute",attribute)
-  if instance[attribute] in tree[attribute].keys():
-    result=tree[attribute][instance[attribute]]
-    print("Instance Attribute:",instance[attribute], "TreeKeys:",tree[attribute].keys())
-    if isinstance(result,dict):
-       return classify(instance,result)
-    else:
-       return result
-  else:
-    return default
-
-
-tree1={'Outlook':['Rainy','Sunny'],'Temperature':['Mild','Hot'],'Humidity':['Normal','High'],'Windy':['Weak','Strong']}
-df2=pd.DataFrame(tree1)
-df2['Predicted']=df2.apply(classify,axis=1, args=(tree,'No'))
-print(df2)
+plt.scatter(bill,tip, color='green')
+plt.plot(xsort[:,1],ypred[SortIndex], color = 'red', linewidth=5)
+plt.xlabel('Total bill')
+plt.ylabel('Tip')
+plt.show();
